1b2158d061888ec8b0286461a345c4bfc80c15e9
LucaLyon1 <45970311+lucalyon1@users.noreply.github.com>
2019-05-22 08:39:11 +0200

    Ajout constructeur

--- src/Serveur.java
+++ src/Serveur.java
@@ -1,8 +1,26 @@
 package src;
 
-public class Serveur {
-    public serveur(){
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 
+public class Serveur {
+    private int port;
+    private ServerSocket servSocket;
+
+    public Serveur(int port, int backlog){
+        try {
+            this.servSocket = new ServerSocket(port, backlog);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Le port est inaccessible");
+        }
+        this.port = port;
     }
+
+    public String recevoir(){
+        String message = new String();
+    }
+
 
 }
