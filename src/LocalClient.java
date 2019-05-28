1ce00aef4c7b40585a87018c64c600a2f8a9c747
CapitainePigeon <lucas69300@outlook.fr>
2019-05-28 23:12:07 +0200

    localclient

--- src/LocalClient.java
+++ src/LocalClient.java
@@ -1,8 +1,8 @@
 package src;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import src.tools.GestionHttpClient;
+
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -13,7 +13,15 @@ public class LocalClient {
         try {
             InetAddress ServeurAdresse= InetAddress.getByName(server_address_str);
             Socket socket = new Socket(ServeurAdresse, Integer.parseInt(server_port_str));
+            BufferedReader buffSocket=new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            //TODO send get
+
+
+
+            //TODO read response
+
+            GestionHttpClient.writeFile(buffSocket,filepath);
 
         } catch (UnknownHostException e) {
             e.printStackTrace();
@@ -32,7 +40,8 @@ public class LocalClient {
             File file = new File(filepath);
             InetAddress ServeurAdresse= InetAddress.getByName(server_address_str);
             Socket socket = new Socket(ServeurAdresse, Integer.parseInt(server_port_str));
-            socket.getInputStream();
+            //socket.getInputStream();
+
 
 
         } catch (UnknownHostException e) {
